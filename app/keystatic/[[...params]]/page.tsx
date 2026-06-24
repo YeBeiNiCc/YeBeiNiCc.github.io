c63aee541a1ad341f460b4b27cf8f52b2cce8c1f
YeBeiNiCc <YeBeiNiCc@users.noreply.github.com>
2026-06-24 23:59:49 +0800

    Use dynamic import for Keystatic admin to avoid SSR issues

--- app/keystatic/[[...params]]/page.tsx
+++ app/keystatic/[[...params]]/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 
-import { makePage } from "@keystatic/next/ui/app";
+import dynamic from "next/dynamic";
 import keystaticConfig from "@/keystatic.config";
 
-export default makePage(keystaticConfig);
+const KeystaticPage = dynamic(
+  () =>
+    import("@keystatic/next/ui/app").then((mod) => {
+      const Page = mod.makePage(keystaticConfig);
+      return Page;
+    }),
+  { ssr: false }
+);
+
+export default function AdminPage() {
+  return <KeystaticPage />;
+}
